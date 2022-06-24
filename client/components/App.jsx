73352df941787bf2a23a3913ef26335e6ea0756c
campbell_may <48898610+Campbell-May@users.noreply.github.com>
2022-06-24 14:59:52 +1200

    added routes

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,71 +1,31 @@
 /* eslint-disable jsx-a11y/alt-text */
 import React, { useState, useEffect } from 'react'
 import {getWeather} from '../api/weatherApi'
+import { Routes, Route } from 'react-router-dom'
+import Windy from './Windy'
+import Home from './Home'
+import Sunny from './Sunny'
+import Rainy from './Rainy'
+import Snowy from './Snowy'
+import Stormy from './Stormy'
+import Cloudy from './Cloudy'
+
 
 function App() {
-  const [weather,setWeather]=useState(null)
 
-
-  const handleMondayClick = () => {
-    return getWeather()
-    .then(obj => {
-      setWeather(obj)
-      
-      })
-  }
-  function valueWeather (){
-    return getWeather()
-    .then(()=>{
-    if (weather?.snowfall_sum[0] ==0 && weather?.rain_sum[0] == 0
-        && weather?.temperature_2m_max[0] > 15 ){
-          return 'sunny'
-        }
-        })
-
-  }
-
-
-  // const checkWeathere =()=>{
-  //   return checkWeather()
-  //     .then(value =>{
-  //       setWeather(value)
-  //     })
-    
-  //   if (weather?.rain_sum[0] > 0 ){
-  //     return 'rainy'
-
-  // }else if (weather?.snowfall_sum[0] > 0){
-  //   return 'snowy'
-  //  if (weather?.snowfall_sum[0] ==0 && weather?.rain_sum[0] == 0
-  //   && weather?.temperature_2m_max[0] > 15 ){
-  //     return 'sunny'
-  
-
-  // }
 
   return( 
     <>
-    <button onClick={handleMondayClick}>Monday</button>
-    <p>{valueWeather}</p>
-    {/* {weather && <p>Welly TIME: {weather?.time[0]}</p>}
-    {(weather?.temperature_2m_min[0]>15)?<p>Welly Weather:sunny </p>:<p>Welly Weather:soooo cold</p>} */}
-    {/* {weather && <p>Welly TempMin: {weather?.temperature_2m_min[0]} </p>} */}
 
-  
-  
-  <div id ='container'>
-    <div id='map'>
-      <img width='1200px' src = 'images/map.png' alt='map picture'/>
-    </div>
-    <div id='windy'>.</div>
-    <div id='sunny'>.</div>
-    <div id='rainy'>.</div>
-    <div id='cloudy'>.</div>
-    <div id='snowy'>.</div>
-    <div id='stormy'>.</div>
-
-  </div>
- 
+ <Routes>
+ <Route path="/" element={<Home />} />
+  <Route path="/windy" element={<Windy />} />
+  <Route path="/Sunny" element={<Sunny />} />
+  <Route path="/Rainy" element={<Rainy />} />
+  <Route path="/Cloudy" element={<Cloudy />} />
+  <Route path="/Snowy" element={<Snowy />} />
+  <Route path="/Stormy" element={<Stormy />} />
+ </Routes>
 </>
   ) 
   }
---
+++ client/components/Stormy.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { useParams } from 'react-router-dom'
+
+function Stormy () {
+
+
+  return (
+  <>
+
+<p>Stormy  </p>
+
+  </>
+  )
+}
+
+export default Stormy
